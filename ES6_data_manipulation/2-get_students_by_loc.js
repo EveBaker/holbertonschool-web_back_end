28c0dcbc5f789c599b9fe18f8973b9001445dce8
EveBaker <Evebaker0162@gmail.com>
2023-09-12 23:30:19 +0000

    backend

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 function getStudentsByLocation(students, city) {
-    return students.filter((student) => student.getStudentsByLocation === city);
+  return students.filter((student) => student.getStudentsByLocation === city);
 }
 export default getStudentsByLocation;
