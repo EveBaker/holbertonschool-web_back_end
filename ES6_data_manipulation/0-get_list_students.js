f1673418b3823c5988568f23d3283a3e47795b51
EveBaker <Evebaker0162@gmail.com>
2023-09-12 22:04:38 +0000

    back end

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,6 @@
-function getListStudents() {
-  return [
+const getListStudents = () => [
     { id: 1, firstname: 'Guillaume', location: 'San Francisco'},
     { id: 2, firstname: 'James', location: 'Columbia' },
     { id: 5, firstname: 'Serena', location: 'San Francisco'},
   ];
-}
+export default getListStudents;
