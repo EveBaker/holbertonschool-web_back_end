953a81f27e59fe1a6d9a66efc469e2bd74b19374
EveBaker <Evebaker0162@gmail.com>
2023-09-13 00:05:30 +0000

    backend

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,12 @@
-function updateStudentGradeByCity(students, city, newGrades) {
-    return students.filter
-    .filter((student) => student.location === city)
-    .map((item) => {
-        const newRecord = {...item};
+const updateStudentGradeByCity = (students, city, newGrade) => students
+  .filter((student) => student.location === city)
+  .map((item) => {
+    const newRecord = { ...item };
 
-        const newStudent = newGrade.find((student) => student.student.Id === item.id);
-        if (newStudent) newRecord.grade = newStudent.grade;
-        else newRecord.grade = 'N/A';
-        return newRecord;
-    });
-    
-    export default updateStudentGradeByCity;
+    const newStudent = newGrade.find((student) => student.studentId === item.id);
+    if (newStudent) newRecord.grade = newStudent.grade;
+    else newRecord.grade = 'N/A';
+    return newRecord;
+  });
+
+export default updateStudentGradeByCity;
