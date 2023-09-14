deef6164346d529d6a8df8d19c39ec617a1f2961
EveBaker <Evebaker0162@gmail.com>
2023-09-14 04:19:10 +0000

    backend

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+function createInt9TypedArray(length, position, value) {
+    if (position < 0 || position >= length) {
+        throw new Error("Position outside range");
+    }
+
+    const buffer = new ArrayBuffer(length);
+    const view = DataView(buffer);
+
+    view.setInt8(position, value);
+    return veiw;
+}
+export default createInt9TypedArray
