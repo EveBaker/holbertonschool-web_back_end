e4781e2a4b5db06fd48c304f041b57ebceb7f5d1
EveBaker <Evebaker0162@gmail.com>
2023-09-14 04:33:11 +0000

    backend

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,15 +1,14 @@
 const createInt8TypedArray = (length, position, value) => {
     if (position >= length || position < 0) {
-        throw new Error('Position outside range');
+      throw new Error('Position outside range');
     }
-
     const int8 = new Int8Array(length);
     int8[position] = value;
-
+  
     const { buffer } = int8;
-    const view = DataView(buffer, 0, length);
-
-    view.setInt8(position, value);
-    return view;
-}
-export default createInt8TypedArray
+    const newView = new DataView(buffer, 0, length);
+  
+    return newView;
+  };
+  
+  export default createInt8TypedArray;
