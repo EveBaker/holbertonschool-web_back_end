9e8254b882bd991743b721f54a22b693e67a6d28
EveBaker <Evebaker0162@gmail.com>
2023-09-16 19:33:54 +0000

    backend

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,12 @@
-import { uploadPhoto, createUser } from './utils';
+import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
-
   return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-        console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
+
